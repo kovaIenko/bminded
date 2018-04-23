35cc5b707ea7ebceb159414a3cb24eb854546839
Ruslan Kovalenko <ruskov004@gmail.com>
2018-04-24 00:35:46 +0300

    Revert "Revert "adding a game""
    This reverts commit 471b77c96556bbfb88e6b61352e5b2ef9fa9f485.

---
+++ target/classes/static/Scripts/LevelFailed0Tries.js
@@ -0,0 +1,31 @@
+var LevelFailed0tries =
+    {
+        preload: function () {
+            game.load.image('LevelFailed0tries', 'Backgrounds/LevelFailed0tries.png');
+            game.load.image('background', 'Backgrounds/orig.jpg');
+            game.load.image('Continue', 'Backgrounds/Contin.png');
+            game.load.image('downPanel', 'Backgrounds/downPanel.png');
+
+        },
+        create : function () {
+            game.add.sprite(0,0,'background');
+            game.add.sprite(117,124,'LevelFailed0tries');
+            var button = game.add.button(225, 300, 'Continue', actionOnClick1, this);
+            game.add.sprite(0,491,'downPanel');
+            game.add.text(300,488,level,{font:'30px Furore',fill:"#525055"});
+            game.add.text(390,488,tries,{font:'30px Furore',fill:"#525055"});
+            game.add.text(420,488,"| Game Points " + gamePoint,{font:'25px Furore',fill:"#525055"});
+
+        },
+        start: function () {
+        }
+    };
+
+function actionOnClick1 () {
+    game.state.start('menu');
+    tries = 3;
+    if(level > 1) {
+        level -= 1;
+
+    }
+}
---
+++ target/classes/static/Scripts/LevelFailedTries.js
@@ -0,0 +1,29 @@
+var LevelFailedtries =
+    {
+        preload: function () {
+            game.load.image('Levelfailedlabel', 'Backgrounds/LevelFailedLabel.png');
+            game.load.image('background', 'Backgrounds/orig.jpg');
+            game.load.image('Continue', 'Backgrounds/Contin.png');
+            game.load.image('downPanel', 'Backgrounds/downPanel.png');
+
+        },
+        create : function () {
+            game.add.sprite(0,0,'background');
+            game.add.sprite(117,124,'Levelfailedlabel');
+            var button = game.add.button(225, 300, 'Continue', actionOnClick, this);
+            game.add.sprite(0,491,'downPanel');
+            game.add.text(300,488,level,{font:'30px Furore',fill:"#525055"});
+            game.add.text(390,488,tries,{font:'30px Furore',fill:"#525055"});
+            game.add.text(370,175,level,{font:'30px Furore',fill:"#525055"});
+            game.add.text(370,210,firefliesAtBegining[level],{font:'25px Furore',fill:"#525055"});
+            game.add.text(370,240,wathcingTime[level]/1000,{font:'25px Furore',fill:"#525055"});
+            game.add.text(420,488,"| Game Points " + gamePoint,{font:'25px Furore',fill:"#525055"});
+
+        },
+        start: function () {
+        }
+    };
+
+function actionOnClick () {
+    game.state.start('play');
+}

88be509146d83981df6cd053bd82aa554d08b5ac
BazaUA <vbazalickyi@gmail.com>
2018-04-24 19:12:10 +0300

    fix route configuration

--- src/main/java/com/bminded/config/AppConfig.java
+++ src/main/java/com/bminded/config/AppConfig.java
@@ -1,23 +1,24 @@
 package com.bminded.config;
 
+import org.springframework.boot.autoconfigure.mustache.web.MustacheViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
-@EnableWebMvc
+
 @Configuration
-public class AppConfig {
+public class AppConfig extends WebMvcConfigurerAdapter {
 
-    @Bean(name = "dataSource")
-    public DriverManagerDataSource dataSource() {
-        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        driverManagerDataSource.setUrl("jdbc:mysql://sl-eu-lon-2-portal.7.dblayer.com:23257/mydb");
-        driverManagerDataSource.setUsername("admin");
-        driverManagerDataSource.setPassword("RYWSYSZYKEAGFKAO");
-        return driverManagerDataSource;
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        super.configurePathMatch(configurer);
+
+        configurer.setUseSuffixPatternMatch(false);
     }
-
 }
---
+++ src/main/java/com/bminded/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.bminded.config;
+
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+}

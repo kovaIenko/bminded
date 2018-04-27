2e4f5059b03084dade8aca016673d116bdd40a2a
BazaUA <vbazalickyi@gmail.com>
2018-04-27 05:19:59 +0300

    add uploud image

--- src/main/java/com/bminded/controller/AccountController.java
+++ src/main/java/com/bminded/controller/AccountController.java
@@ -1,6 +1,5 @@
 package com.bminded.controller;
 
-import com.bminded.dto.UserDTO;
 import com.bminded.entity.UserEntity;
 import com.bminded.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +7,26 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class AccountController {
 
     @Autowired
     UserService userService;
+    @Autowired
+    private HttpServletRequest request;
 
     @GetMapping(value = "/account")
     public ModelAndView cabinet() {
@@ -28,18 +39,42 @@ public class AccountController {
         modelAndView.addObject("second_name", user.getSecond_name());
         modelAndView.addObject("email", user.getEmail());
 
-        if (user.getPhoto_ref()==null)
+        if (user.getPhoto_ref() == null)
             modelAndView.addObject("photo", "/images/default-avatar.png");
-       else
+        else
             modelAndView.addObject("photo", user.getPhoto_ref());
 
-    return modelAndView;
+        return modelAndView;
 
     }
+
     @GetMapping("/statistic")
     public ModelAndView statistic() {
         ModelAndView modelAndView = new ModelAndView("statistic");
         return modelAndView;
+    }
+
+    @PostMapping("/upload")
+    public RedirectView singleFileUpload(@RequestParam("file") MultipartFile file,
+                                         RedirectAttributes redirectAttributes) {
+        String filePath = request.getServletContext().getRealPath("/ ");
+        if (file.isEmpty()) {
+            System.out.print("error");
+        }
+
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(filePath + file.getOriginalFilename());
+            Files.write(path, bytes);
+            //href to save
+            System.out.println(filePath + file.getOriginalFilename());
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new RedirectView("/account");
     }
 
 
